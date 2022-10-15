f5db5aa8e346729e0dab3924ec1b9b33480f506d
aygul.okdirova <aygul.okdirova@check24.de>
2022-10-15 20:03:31 +0200

    redux tools configured

--- src/store/store.js
+++ src/store/store.js
@@ -4,8 +4,20 @@ import { rootReducer } from './root-reducer';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-const middleWares = [logger];
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+//applying middleware only inside development
+const middleWares = [
+	process.env.NODE_ENV === 'development' && logger,
+].filter(Boolean);
+
+const composeEnhancer =
+	(process.env.NODE_ENV === 'development' &&
+		window &&
+		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+	compose;
+
+const composedEnhancers = composeEnhancer(
+	applyMiddleware(...middleWares)
+);
 
 const persistConfig = {
 	key: 'root',
